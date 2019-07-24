e0dccbb2422bf5fb8da6a8711518782150128c3e
openwell <timileyinojo@yahoo.com>
2019-07-24 15:41:07 +0100

    create user routes

---
+++ src/user/user-route.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const db = require("./user-model");
+const auth = require("./user-middleware");
+
+const router = express.Router();
+
+router.post("/register", auth.validateUser, (req, res) => {
+  const { username, password } = req.body;
+
+  const data = {
+    username: username,
+    password: auth.myBcrypt(password, 10)
+  };
+  db.createUser(data)
+    .then(dbResponse => {
+      return res.status(200).json({
+        data: dbResponse
+      });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.get("/users", auth.restriction, (req, res) => {
+  db.getUsers()
+    .then(dbResponse => {
+      return res.status(200).json({
+        data: dbResponse
+      });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.post(
+  "/login",
+  auth.validateUser,
+  auth.validateUserPassword,
+  (req, res) => {
+    res.status(200).json({message: 'Welcome'})
+  }
+);
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("you can never leave");
+      } else {
+        res.send("bye, thanks!");
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
+module.exports = router;
